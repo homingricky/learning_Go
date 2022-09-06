d90fab53d175250f785ccd8f3882940de8a42fd0
homingricky <homingricky@gmail.com>
2022-09-06 21:12:24 +0800

    feat: variables

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt" //package must be used after imported
+)
 
-// entry point
+// entry point of the application
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference" //variables must be used after declared
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to our", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets to attend")
+
 }
